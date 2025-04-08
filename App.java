c42d1ae1fb45661befa98724c4aea8169b14e3d6
sanjana-110 <sanjanawadeyar500@gmail.com>
2025-04-08 17:16:15 +0530

    save the changes

---
+++ App.java
@@ -0,0 +1,9 @@
+public class App{
+    public static void main(String[] args){
+
+        System.out.println("Hello world");
+        System.out.println("sanjana");
+         System.out.println("sanjanA");
+        
+    }
+}
